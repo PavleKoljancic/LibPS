d6a806e7fd17ee1a6a157b10a6818391bb416825
kezija99 <dejankezic99@gmail.com>
2023-03-29 22:53:29 +0200

    dodane ostale klase

---
+++ app/src/main/java/lib/etickets/users/administrator/Administrator.java
@@ -0,0 +1,25 @@
+package lib.etickets.users.administrator;
+
+import lib.etickets.users.generaluser.GeneralUser;
+
+public class Administrator extends GeneralUser{
+    List<TerminalActivationRequest> terminalActivationRequests;
+
+    public Administrator() {
+    }
+
+    public Administrator(String userId, String email, String firstName, String lastName,
+            List<TerminalActivationRequest> terminalActivationRequests) {
+        super(userId, email, firstName, lastName);
+        this.terminalActivationRequests = terminalActivationRequests;
+    }
+
+    public List<TerminalActivationRequest> getTerminalActivationRequests() {
+        return terminalActivationRequests;
+    }
+
+    public void setTerminalActivationRequests(List<TerminalActivationRequest> terminalActivationRequests) {
+        this.terminalActivationRequests = terminalActivationRequests;
+    }
+    
+}
